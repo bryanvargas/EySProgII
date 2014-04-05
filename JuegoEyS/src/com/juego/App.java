e97f1f1c5e59e13f84110831392140cded5c90a2
Rodolfo Vargas <bryanprsvar@gmail.com>
2014-04-05 13:06:46 -0300

    Se cambio nombre de Clase App

--- JuegoEyS/src/com/juego/App.java
+++ JuegoEyS/src/com/juego/App.java
@@ -3,6 +3,7 @@ package com.juego;
 
 
 public final class App {
+	//Se cambio Nombre de la clase App
 	
 	 public static void main ( String [] args ) {	
 	 String [] nombreJugadores = {"Bryan", "Alvaro"};
